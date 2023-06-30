becd65bf76f29af454be651f51aca83647555ef3
Natika31 <natika.orsas@gmail.com>
2023-06-30 10:54:28 +0200

    gradle

---
+++ src/main/java/com/projet/edp/EdpApplication.java
@@ -0,0 +1,13 @@
+package com.projet.edp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/edp/EdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.edp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
