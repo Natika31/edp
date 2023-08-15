b7981aaa531691d084ea8feab5a534ff6d6b3cd7
Natika31 <natika.orsas@gmail.com>
2023-08-15 09:50:09 +0200

    DTO
    adapt controller

--- src/main/java/com/projet/edp/fileTree/ui/FileTreeController.java
+++ src/main/java/com/projet/edp/fileTree/ui/FileTreeController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.projet.edp.fileTree.domain.FileTreeItem;
+import com.projet.edp.fileTree.domain.TreeItem;
+import com.projet.edp.fileTree.dto.TreeItemDTO;
+import com.projet.edp.fileTree.dto.TreeDTOConversion;
 import com.projet.edp.fileTree.service.FileTreeService;
 
 @RestController
@@ -16,20 +18,25 @@ public class FileTreeController {
 	
 	@Autowired
 	FileTreeService fileTreeService;
+	
+	private TreeDTOConversion itemDTOConversion;
 
 	public FileTreeController(FileTreeService fileTreeService) {
 		this.fileTreeService = fileTreeService;
+		this.itemDTOConversion = new TreeDTOConversion();
 	}
 	
 	@PostMapping("/api/file-tree/save")
-	public ResponseEntity<FileTreeItem> create(@RequestBody FileTreeItem fileTreeItem) {
-		fileTreeService.save(fileTreeItem);
-		return new ResponseEntity<FileTreeItem>(fileTreeItem, HttpStatus.CREATED);
+	public ResponseEntity<TreeItemDTO> create(@RequestBody TreeItemDTO fileTreeItemDTO) {
+		TreeItem treeItem = itemDTOConversion.convertDTOtoEntities(fileTreeItemDTO);
+		fileTreeService.save(treeItem);
+		return new ResponseEntity<TreeItemDTO>(fileTreeItemDTO, HttpStatus.CREATED);
 	}
 
 	@GetMapping("/api/file-tree")
-	public ResponseEntity<FileTreeItem> getItemById(@RequestParam Long item_id) {
-		FileTreeItem fileTreeItem = fileTreeService.findFileTreeItemById(item_id).get();
-		return ResponseEntity.ok(fileTreeItem);
+	public ResponseEntity<TreeItemDTO> getItemById(@RequestParam String item_id) {
+		TreeItem treeItem = fileTreeService.findFileTreeItemById(Long.valueOf(item_id)).get();
+		TreeItemDTO fileTreeItemDTO = itemDTOConversion.convertEntityToDTO(treeItem);
+		return ResponseEntity.ok(fileTreeItemDTO);
 	}
 }
--- src/test/java/com/projet/edp/fileTreeTest/FileTreeRestControllerTest.java
+++ src/test/java/com/projet/edp/fileTreeTest/FileTreeRestControllerTest.java
@@ -12,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import com.projet.edp.fileTree.domain.FileTreeItem;
+
+import com.projet.edp.fileTree.domain.Directory;
+import com.projet.edp.fileTree.domain.FileContent;
+import com.projet.edp.fileTree.domain.MyFile;
+import com.projet.edp.fileTree.domain.TreeItem;
 import com.projet.edp.fileTree.service.FileTreeService;
 import com.projet.edp.fileTree.ui.DirectoryController;
 import com.projet.edp.fileTree.ui.FileTreeController;
@@ -27,13 +31,80 @@ class FileTreeRestControllerTest {
 	private FileTreeService fileTreeService;
 
 	@Test
-	void GivenSelectedDirectory_whenRequestGETEmptyDirectoryIdEquals1_thenGetStoredDirectoryIdEquals1() throws Exception {
-		FileTreeItem emptyCompositeItem = new FileTreeItem("/home/", "home");
-		emptyCompositeItem.setItem_id(1L);
-		when(fileTreeService.findFileTreeItemById(1L)).thenReturn(Optional.of(emptyCompositeItem));
-		this.mockMvc.perform(get("/api/file-tree?item_id=1")).andDo(print()).andExpect(status().isOk())
-		.andExpect(content().string(containsString("item_id\":1,\"item_local_path\":\"/home/\",\"item_name\":\"home\"")))
+	void GivenEmptyDirectoryId1_whenHTTPRequestGETTreeItemId1_thenGetStoredDirectoryIdEquals1() throws Exception {
+		TreeItem rootDirectory = new Directory("home", "/home/");
+		rootDirectory.setItem_id(1L);
+		when(fileTreeService.findFileTreeItemById(1L)).thenReturn(Optional.of(rootDirectory));
+
+		this.mockMvc.perform(get("/api/file-tree?item_id=1")).andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("item_id\":\"1\",\"item_local_path\":\"/home/\",\"item_name\":\"home\"")));
+	}
+
+	@Test
+	void GivenDirectoryId1ContainsDirectoryId2_whenRequestGETTreeItemId1_thenGetDirectoryId1ContainsDirectoryId2() throws Exception {
+		Directory rootDirectory = new Directory("home", "/home/");
+		rootDirectory.setItem_id(1L);
+		Directory childDirectory = new Directory("Henri Salvador", "/home/henri_salvador");
+		childDirectory.setItem_id(2L);
+		rootDirectory.addChildren(childDirectory);
+		
+		
+		when(fileTreeService.findFileTreeItemById(1L)).thenReturn(Optional.of(rootDirectory));
+		this.mockMvc.perform(get("/api/file-tree?item_id=1")).andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().string(containsString("item_id\":\"1\",\"item_local_path\":\"/home/\",\"item_name\":\"home\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":["
+				+ "{\"item_id\":\"2\",\"item_local_path\":\"/home/henri_salvador\",\"item_name\":\"Henri Salvador\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":[]}"
+				+ "]")))
 		;
+	}
+	
+	@Test
+	void GivenDirectoryId1ContainsDirectoryId2ContainsFileId3_whenRequestGETTreeItemId1_thenGetDirectoryId1ContainsDirectoryId2ContainsFileId3() throws Exception {
+		Directory rootDirectory = new Directory("home", "/home/");
+		rootDirectory.setItem_id(1L);
+		Directory childDirectory = new Directory("Henri Salvador", "/home/henri_salvador");
+		childDirectory.setItem_id(2L);
+		FileContent fileContent = new FileContent();
+		byte[] binaryArray = fileContent.convertInputFileToBinaryArray("C:/Users/Natacha/Documents/cnam/GLG204 - 2023/DANS MON ILE.pdf");
+		fileContent.setBinary_content(binaryArray);
+		TreeItem childFile = new MyFile("Dans mon ile", "/home/henri_salvador/dans_mon_ile.pdf","pdf","C:/Users/Natacha/Documents/cnam/GLG204 - 2023/DANS MON ILE.pdf", fileContent );
+		childFile.setItem_id(3L);
+		childDirectory.addChildren(childFile);
+		rootDirectory.addChildren(childDirectory);
+		when(fileTreeService.findFileTreeItemById(1L)).thenReturn(Optional.of(rootDirectory));
+		this.mockMvc.perform(get("/api/file-tree?item_id=1")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString(
+				  "\"item_id\":\"1\",\"item_local_path\":\"/home/\",\"item_name\":\"home\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":["
+				+ "{\"item_id\":\"2\",\"item_local_path\":\"/home/henri_salvador\",\"item_name\":\"Henri Salvador\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":["
+				+ "{\"item_id\":\"3\",\"item_local_path\":\"/home/henri_salvador/dans_mon_ile.pdf\",\"item_name\":\"Dans mon ile\",\"item_type\":\"class com.projet.edp.fileTree.domain.MyFile\",\"children\":[]}"
+				+ "]}"
+				+ "]")));
+	}
+	
+	@Test
+	void GivenDirectoryId1ContainsDirectoryId2AndDirectoryId3ContainsFileId4_whenRequestGETTreeItemId1_thenGetDirectoryId1ContainsDirectoryId2ContainsFileId3() throws Exception {
+		Directory rootDirectory = new Directory("home", "/home/");
+		rootDirectory.setItem_id(1L);
+		Directory childDirectory1 = new Directory("Henri Salvador", "/home/henri_salvador");
+		childDirectory1.setItem_id(2L);
+		Directory childDirectory2 = new Directory("Natacha", "/home/henri_salvador/natacha/");
+		childDirectory2.setItem_id(3L);
+		FileContent fileContent = new FileContent();
+		byte[] binaryArray = fileContent.convertInputFileToBinaryArray("C:/Users/Natacha/Documents/cnam/GLG204 - 2023/DANS MON ILE.pdf");
+		fileContent.setBinary_content(binaryArray);
+		TreeItem childFile = new MyFile("Dans mon ile", "/home/henri_salvador/dans_mon_ile.pdf","pdf","C:/Users/Natacha/Documents/cnam/GLG204 - 2023/DANS MON ILE.pdf", fileContent );
+		childFile.setItem_id(3L);
+		childDirectory1.addChildren(childFile);
+		rootDirectory.addChildren(childDirectory1);
+		when(fileTreeService.findFileTreeItemById(1L)).thenReturn(Optional.of(rootDirectory));
+		this.mockMvc.perform(get("/api/file-tree?item_id=1")).andDo(print()).andExpect(status().isOk())
+		.andExpect(content().string(containsString(
+				  "\"item_id\":\"1\",\"item_local_path\":\"/home/\",\"item_name\":\"home\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":["
+				+ "{\"item_id\":\"2\",\"item_local_path\":\"/home/henri_salvador\",\"item_name\":\"Henri Salvador\",\"item_type\":\"class com.projet.edp.fileTree.domain.Directory\",\"children\":["
+				+ "{\"item_id\":\"3\",\"item_local_path\":\"/home/henri_salvador/dans_mon_ile.pdf\",\"item_name\":\"Dans mon ile\",\"item_type\":\"class com.projet.edp.fileTree.domain.MyFile\",\"children\":[]}"
+				+ "]}"
+				+ "]")));
 	}
 
 }
