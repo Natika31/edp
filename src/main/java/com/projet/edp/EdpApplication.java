3408e169fecc8a085f2c1e239f362e40f092c205
Natika31 <natika.orsas@gmail.com>
2023-09-15 14:29:50 +0200

    implement Basic Authentication
    add spring-boot-starter-security dependency
    exclude AutoConfig and configure HttpAuthorizations
    create a PoC Bean and controller
    customize login and password

--- src/main/java/com/projet/edp/EdpApplication.java
+++ src/main/java/com/projet/edp/EdpApplication.java
@@ -2,11 +2,34 @@ package com.projet.edp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.modelmapper.ModelMapper;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EdpApplication {
+	
+	@Configuration
+	@EnableWebSecurity
+	public class BasicSecurityConfiguration {
+
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http.csrf(csrf -> csrf.disable())
+	            .authorizeHttpRequests(
+	            		(authz)-> authz
+	    	            .requestMatchers(HttpMethod.OPTIONS, "/**")
+	    	            .permitAll()
+	    	            .anyRequest()
+	    	            .authenticated()
+	            		)
+	            .httpBasic(Customizer.withDefaults());
+	    }
+	}
 	
 	@Bean
 	public ModelMapper modelMapper() {
